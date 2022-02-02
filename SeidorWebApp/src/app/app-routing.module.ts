2aab254a714f45e4c08fdc7747b161876b72a8e7
Everson Vieira Souza vieira <Everson-SV@hotmail.com>
2022-02-02 19:23:44 -0300

    02022022

--- SeidorWebApp/src/app/app-routing.module.ts
+++ SeidorWebApp/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CpfComponent } from './Pages/cpf/cpf.component';
 import { HomeComponent } from './Pages/home/home.component';
-import { LogComponent } from './Pages/log/log.component';
+import { LogComponent } from './Pages/logPage/log.component';
 import { UserRegisterComponent } from './Pages/user-register/user-register.component';
 import { UserUpdateComponent } from './Pages/user-update/user-update.component';
 
